fb6cbd992f5013be01e16dfd5a7ce6b8d448f16b
jillwilliams <codeGeek.03.14@gmail.com>
2019-11-01 13:52:37 -0700

    draft

---
+++ script.js
@@ -0,0 +1,18 @@
+const myProblem = () => {
+    return "y = x/7 - 3/7";
+};
+const myCalc = () =>{
+    document.getElementById("demo").innerHTML = myProblem();
+};
+const myProb2 = ( ) =>{
+    return "y = 5x - 4";
+};
+const myCalc2 = () => {
+    document.getElementById("demo1").innerHTML = myProb2();
+};
+const myProb3 = ( ) =>{
+    return "y = 2x + 6";
+};
+const myCalc3 = () => {
+    document.getElementById("demo2").innerHTML = myProb3();
+};
